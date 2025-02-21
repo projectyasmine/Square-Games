25abcbf388182cb0000d909814bbd00a62990a8a
Yasmine CHAIBOUBI <yasmine.chaiboubi@digital-grenoble.local>
2025-02-21 08:59:18 +0100

    commit final

--- src/main/java/com/squaregames/demo/dao/UserEntityRepository.java
+++ src/main/java/com/squaregames/demo/dao/UserEntityRepository.java
@@ -2,7 +2,5 @@ package com.squaregames.demo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
-
 public interface UserEntityRepository extends JpaRepository<UserEntity, String> {
 }
